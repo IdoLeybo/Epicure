2a15b05f4c83f105c171304a91063fbc50d6c2dc
IdoLeybo <Idol@moveo.co.il>
2021-10-19 19:06:41 +0300

    Added functions.php & scripts.js files

---
+++ functions.php
@@ -0,0 +1,128 @@
+<?php
+
+// Add Setup
+function epicure_setup() {
+
+}
+add_action('init', 'epicure_setup');
+
+// Add Menus
+function epicure_menus() {
+    register_nav_menus(array(
+        'header-menu' => __('Header Menu', 'epicure'),
+    ));
+}
+add_action('init', 'epicure_menus');
+
+// Add Scripts
+function epicure_scripts() {
+    //Register Styles
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '1.0.0');
+    wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0');
+
+
+    //Enqueue Styles
+    wp_enqueue_style('style');
+    wp_enqueue_style('fontawesome');
+
+
+    //Register Scripts
+    wp_register_script('script', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
+
+    //Enqueue Scripts
+    wp_enqueue_script('script');
+
+}
+add_action('wp_enqueue_scripts', 'epicure_scripts');
+
+// Creates a Widget Zone
+function epicure_widgets() {
+    register_sidebar( array(
+        'name' =>  'header',
+        'id' => 'header',
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
+}
+add_action('widgets_init', 'epicure_widgets');
+
+//Post Type
+function epicure_dishes() {
+    $labels = array(
+        'name'               => _x( 'Dishes', 'epicure' ),
+        'singular_name'      => _x( 'Dish', 'post type singular name', 'epicure' ),
+        'menu_name'          => _x( 'Dishes', 'admin menu', 'epicure' ),
+        'name_admin_bar'     => _x( 'Dishes', 'add new on admin bar', 'epicure' ),
+        'add_new'            => _x( 'Add New', 'book', 'epicure' ),
+        'add_new_item'       => __( 'Add New Dish', 'epicure' ),
+        'new_item'           => __( 'New Dishes', 'epicure' ),
+        'edit_item'          => __( 'Edit Dishes', 'epicure' ),
+        'view_item'          => __( 'View Dishes', 'epicure' ),
+        'all_items'          => __( 'All Dishes', 'epicure' ),
+        'search_items'       => __( 'Search Dishes', 'epicure' ),
+        'parent_item_colon'  => __( 'Parent Dishes:', 'epicure' ),
+        'not_found'          => __( 'No Dishes found.', 'epicure' ),
+        'not_found_in_trash' => __( 'No Dishes found in Trash.', 'epicure' )
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'description'        => __( 'Description.', 'epicure' ),
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'dishes' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 6,
+        'supports'           => array( 'title', 'editor', 'thumbnail' ),
+        'taxonomies'          => array( 'category' ),
+    );
+
+    register_post_type( 'dishes', $args );
+}
+add_action( 'init', 'epicure_dishes' );
+
+function epicure_restaurants() {
+    $labels = array(
+        'name'               => _x( 'Restaurants', 'epicure' ),
+        'singular_name'      => _x( 'Restaurant', 'post type singular name', 'epicure' ),
+        'menu_name'          => _x( 'Restaurants', 'admin menu', 'epicure' ),
+        'name_admin_bar'     => _x( 'Restaurants', 'add new on admin bar', 'epicure' ),
+        'add_new'            => _x( 'Add New', 'book', 'epicure' ),
+        'add_new_item'       => __( 'Add New Restaurant', 'epicure' ),
+        'new_item'           => __( 'New Restaurants', 'epicure' ),
+        'edit_item'          => __( 'Edit Restaurants', 'epicure' ),
+        'view_item'          => __( 'View Restaurants', 'epicure' ),
+        'all_items'          => __( 'All Restaurants', 'epicure' ),
+        'search_items'       => __( 'Search Restaurants', 'epicure' ),
+        'parent_item_colon'  => __( 'Parent Restaurants:', 'epicure' ),
+        'not_found'          => __( 'No Restaurants found.', 'epicure' ),
+        'not_found_in_trash' => __( 'No Restaurants found in Trash.', 'epicure' )
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'description'        => __( 'Description.', 'epicure' ),
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'restaurants' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 6,
+        'supports'           => array( 'title', 'editor', 'thumbnail' ),
+        'taxonomies'          => array( 'category' ),
+    );
+
+    register_post_type( 'restaurants', $args );
+}
+add_action( 'init', 'epicure_restaurants' );
