9aa8b116bd33e0e95adae8486e0aefea8ecbb8ed
IdoLeybo <Idol@moveo.co.il>
2021-11-11 13:44:04 +0200

    PUSH

--- web/app/themes/epicure/webpack.config.js
+++ web/app/themes/epicure/webpack.config.js
@@ -1,97 +1,82 @@
-// webpack.config.js
-/**
- * Webpack configuration.
- */
-const path = require( 'path' );
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isProduction = process.env.NODE_ENV == "production";
+const stylesHandler = MiniCssExtractPlugin.loader;
 var HashPlugin = require('hash-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
-const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
-const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
-// JS Directory path.
-const JS_DIR = path.resolve( __dirname, 'src/js' );
+// the path(s) that should be cleaned
 let pathsToClean = [
-    'dist'
+  'dist'
 ];
+// the clean options to use
 let cleanOptions = {
-    root: path.resolve(__dirname, ''),
-    verbose: true,
-    dry: false
+  root: path.resolve(__dirname, ''),
+  verbose: true,
+  dry: false
 };
-const plugins = [
-    new CleanWebpackPlugin( {pathsToClean}, cleanOptions ),
-    new ExtractTextPlugin({
-        filename: '[name].css',
+
+const config = {
+  entry: {
+    admin: "./js/admin_ajax.js",
+    ajax: "./js/ajax.js",
+    filters: "./js/filters.js",
+    popup: "./js/popup.js",
+    quantity: "./js/quantity.js",
+    reservationDB: "./js/reservation-DB.js",
+    scripts: "./js/scripts.js",
+    sweetalert2: "./js/sweetalert2.min.js",
+    userDB: "./js/user-DB.js",
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: path.resolve(__dirname, 'resources')
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery',
+      IScroll: 'iscroll'
     }),
+    new MiniCssExtractPlugin(),
     new HashPlugin({path: './build', fileName: 'version-hash.txt'}),
+    new CleanWebpackPlugin(pathsToClean, cleanOptions),
     new CompressionPlugin()
-];
-const rules = [
-    {
-        test: /\.js$/,
-        include: [ JS_DIR ],
-        exclude: /node_modules/,
-        use: 'babel-loader'
-    },
-    {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-        ]
-    },
-    {
-        test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
-        use: {
-            loader: 'file-loader',
-            options: {
-                name: '[path][name].[ext]',
-                publicPath: 'production' === process.env.NODE_ENV ? '../' : '../../'
-            }
-        }
-    },
-];
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
 
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
 
-module.exports = env  => {
-
-    return {
-
-        entry: {
-            main: './js/scripts.js',
-            sweetalert: './js/sweetalert2.min.js',
-            adminside: './js/admin_ajax.js',
-            filters: './js/filters.js'
-        },
-        output: {
-            filename: '[name].[hash].js',
-            path: path.resolve(__dirname, 'dist'),
-            publicPath: path.resolve(__dirname, 'resources')
-        },
-        mode: 'development',
-        module: {
-            rules: rules,
-        },
-        optimization: {
-            minimizer: [
-                new OptimizeCssAssetsPlugin( {} ),
-                new UglifyJsPlugin( {
-                    cache: true,
-                    parallel: true,
-                    sourceMap: false
-                } )
-            ]
-        },
-        plugins: plugins,
-        watch: false,
-        node: {
-            fs: 'empty'
-        },
-        externals: {
-            jquery: 'jQuery'
-        }
-    }
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
